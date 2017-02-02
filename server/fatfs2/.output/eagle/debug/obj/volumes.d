8e283a4806dca6dda500bcd5758c8417a9b5d9b2
patlas <plas@student.agh.edu.pl>
2017-02-03 03:33:56 +0800

    Remove .output directory

--- server/fatfs2/.output/eagle/debug/obj/volumes.d
+++
@@ -1,3 +0,0 @@
-.output/eagle/debug/obj/volumes.o .output/eagle/debug/obj/volumes.d : volumes.c volumes.h \
- /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/stdint.h \
- /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/stddef.h
